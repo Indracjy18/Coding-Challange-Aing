ca2c95ca82fa3cf5b3ad01ad39ca41a1ed8f970d
unknown <indracahyajaty@gmail.com>
2024-03-17 19:16:47 +0700

    first test day 1

---
+++ 1-EventOrOdd.js
@@ -0,0 +1,12 @@
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+// Contoh penggunaan fungsi:
+let inputNumber = parseInt;
+let result = evenOrOdd(inputNumber);
+console.log("Bilangan tersebut adalah", result);
